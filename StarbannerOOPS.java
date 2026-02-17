ad0b8141a73faf9bc967df3e185ba511e842b53a
ritesh2007-tech <riteshrathanlal30@gmail.com>
2026-02-17 08:27:52 +0530

    StarbannerOOPS.java with perfect OOPS star banner

---
+++ StarbannerOOPS.java
@@ -0,0 +1,12 @@
+public class StarbannerOOPS {
+    public static void main(String[] args) {
+
+    System.out.println(String.join("  ", " ***** ", " ***** ", "****** ", " ***** "));
+    System.out.println(String.join("  ", "*     *", "*     *", "*     *", "*     *"));
+    System.out.println(String.join("  ", "*     *", "*     *", "*     *", "*      "));
+    System.out.println(String.join("  ", "*     *", "*     *", "****** ", " ***** "));
+    System.out.println(String.join("  ", "*     *", "*     *", "*      ", "      *"));
+    System.out.println(String.join("  ", "*     *", "*     *", "*      ", "*     *"));
+    System.out.println(String.join("  ", " ***** ", " ***** ", "*      ", " ***** "));
+    }
+}
